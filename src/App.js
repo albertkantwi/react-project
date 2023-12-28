92291e78f27d4716adf30616ba22788ab8e17119
unknown <albertkantwi@gmail.com>
2023-12-28 23:58:59 +0000

    Fetch data with Api

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import Axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(0);
+
+  const fetchData = () => {
+    Axios.get(`https:api.agify.io/?name=${name}`).then((res) => {
+      setPredictedAge(res.data.age);
+    });
+  };
 
   return (
     <div className="App">
-      <button>Predict Age</button>
+      <input
+        placeholder="Insert name..."
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <button onClick={fetchData}>Predict Age</button>
+
+      <h1>Predicted Age: {predictedAge}</h1>
     </div>
   );
 }
