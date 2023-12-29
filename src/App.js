0f63cf9aabf30676f17ee22a678675214dd3e2f7
unknown <albertkantwi@gmail.com>
2023-12-29 01:49:54 +0000

    Update App with routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import "./App.css";
-import Axios from "axios";
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./pages/Navbar";
 
 function App() {
-  const [name, setName] = useState("");
-  const [predictedAge, setPredictedAge] = useState(null);
-
-  const fetchData = () => {
-    Axios.get(`https:api.agify.io/?name=${name}`).then((res) => {
-      setPredictedAge(res.data);
-    });
-  };
-
+  
   return (
     <div className="App">
-      <input
-        placeholder="Insert name..."
-        onChange={(event) => {
-          setName(event.target.value);
-        }}
-      />
-      <button onClick={fetchData}>Predict Age</button>
-
-      <h1>Name: {predictedAge?.name}</h1>
-      <h1>Predicted Age: {predictedAge?.age}</h1>
-      <h1>Count: {predictedAge?.count}</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/*"  element={<h1>Page not Found</h1>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
