bba40a97edc6d29493df53b50f0df3d6689bb91f
unknown <albertkantwi@gmail.com>
2023-12-30 23:50:07 +0000

    Update home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
-export const Home = () => {
+export const Home = (props) => {
   return (
     <div>
       <h1>Page 1</h1>
-      <h2>This is HOME page</h2>
+      <h2>This is HOME page by: {props.username}</h2>
     </div>
   );
 };
