a1bf2f589eff68897bad1499b41ef8f337395d09
unknown <albertkantwi@gmail.com>
2023-12-31 23:02:20 +0000

    Update app file

--- src/App.js
+++ src/App.js
@@ -8,16 +8,19 @@ import { useState } from "react";
 
 function App() {
   const [username, setUsername] = useState("AlbertTech");
-  
+
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home username={username}/>}/>
-          <Route path="/profile" element={<Profile username={username} setUsername={setUsername}/>} />
+          <Route path="/" element={<Home username={username} />} />
+          <Route
+            path="/profile"
+            element={<Profile username={username} setUsername={setUsername} />}
+          />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/*"  element={<h1>Page not Found</h1>}/>
+          <Route path="/*" element={<h1>Page not Found</h1>} />
         </Routes>
       </Router>
     </div>
