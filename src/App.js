906e691d512de598b1b530c010b5b4de41c584f2
unknown <albertkantwi@gmail.com>
2024-01-02 23:14:42 +0000

    Add Navbar to routes

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home username={username} />} />
           <Route
