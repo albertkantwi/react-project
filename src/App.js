734e54c37f35626e06955a1dc445a2cb09370d12
unknown <albertkantwi@gmail.com>
2024-01-04 22:37:56 +0000

    Delete contact page from router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Profile } from "./pages/Profile";
-import { Contact } from "./pages/Contact";
 import { Navbar } from "./pages/Navbar";
 import { useState } from "react";
 
