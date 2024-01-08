fa7107d9af6fe8c3a99d37faeeb37bf26cd27079
unknown <albertkantwi@gmail.com>
2024-01-08 23:08:39 +0000

    Add pages

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import { Home } from "./pages/Home";
 import { Profile } from "./pages/Profile";
 import { Contact } from "./pages/Contact";
 import { Navbar } from "./pages/Navbar";
-import { useState } from "react";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 
 function App() {
-  const [username, setUsername] = useState("AlbertTech");
 
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home username={username} />} />
+          <Route path="/" element={<Home  />} />
           <Route
             path="/profile"
-            element={<Profile username={username} setUsername={setUsername} />}
+            element={<Profile  />}
           />
           <Route path="/contact" element={<Contact />} />
           <Route path="/*" element={<h1>Page not Found</h1>} />
--- src/components/ChangeProfile.js
+++ src/components/ChangeProfile.js
@@ -1,22 +1,11 @@
-import { useState } from "react";
 
-export const ChangeProfile = (props) => {
-  const [newUsername, setNewUsername] = useState("");
+export const ChangeProfile = () => {
 
   return (
     <div>
-      <input
-        onChange={(event) => {
-          setNewUsername(event.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          props.setUsername(newUsername);
-        }}
-      >
+      
         Change Username
-      </button>
+      
     </div>
   );
 };
