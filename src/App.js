16142cf4542c475ac6366e57e37fa3585e968d2d
unknown <albertkantwi@gmail.com>
2024-01-14 02:52:22 +0000

    Add form page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Profile } from "./pages/Profile";
-import { Contact } from "./pages/Contact";
-import { Navbar } from "./pages/Navbar";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import {Form} from "./pages/Form"
+
 
 function App() {
-  const client = new QueryClient();
-
   return (
     <div className="App">
-      <QueryClientProvider client={client}>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home  />} />
-          <Route
-            path="/profile"
-            element={<Profile  />}
-          />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/*" element={<h1>Page not Found</h1>} />
-        </Routes>
-      </Router>
-      </QueryClientProvider>
+      <Form />
     </div>
   );
 }
